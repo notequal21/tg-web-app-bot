3d9d6387288ec65f56e96f64dbfb5fa3d631425c
Kirill Makhnev <kalorigen@gmail.com>
2024-02-07 12:32:54 +0300

    added gitignore

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
 
 const token = '6904460119:AAEPp6vAwWGPzP59-JigozQ2jJpXem2D8mw';
-const webAppUrl = 'https://tranquil-moonbeam-6811fd.netlify.app/';
+const webAppUrl = 'https://tranquil-moonbeam-6811fd.netlify.app';
 const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
@@ -14,7 +20,9 @@ bot.on('message', async (msg) => {
       'Заходи в наш интернет магазин по кнопке ниже',
       {
         reply_markup: {
-          keyboard: [[{ text: 'Заполни форму', web_app: { url: webAppUrl } }]],
+          keyboard: [
+            [{ text: 'Заполни форму', web_app: { url: webAppUrl + '/form' } }],
+          ],
         },
       }
     );
@@ -27,4 +35,52 @@ bot.on('message', async (msg) => {
       },
     });
   }
+
+  if (msg?.web_app_data?.data) {
+    try {
+      const data = JSON.parse(msg?.web_app_data?.data);
+
+      await bot.sendMessage(chatId, 'Thanks');
+      await bot.sendMessage(chatId, 'Your country: ' + data?.country);
+      await bot.sendMessage(chatId, 'Your street: ' + data?.street);
+
+      setTimeout(async () => {
+        await bot.sendMessage(
+          chatId,
+          'You receive all information in this bot'
+        );
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 });
+
+app.post('/web-data', async (req, res) => {
+  const { queryId, products, totalPrice } = req.data;
+
+  try {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Payment Successful',
+      input_message_content: {
+        message_text: `Thanks for buy for a sum ${totalPrice}`,
+      },
+    });
+    return res.status(200).json({});
+  } catch (error) {
+    await bot.answerWebAppQuery(queryId, {
+      type: 'article',
+      id: queryId,
+      title: 'Payment Error',
+      input_message_content: {
+        message_text: `Payment Error`,
+      },
+    });
+    return res.status(500).json({});
+  }
+});
+
+const PORT = 8000;
+app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
